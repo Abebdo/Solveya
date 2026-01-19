b4a0ae4b1c020ade9ed47c443b985e916cbe02af
Abebdo <xjokerrgamer@gmail.com>
2026-01-19 18:58:30 +0200

    Add Header and Footer components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { ScamChecker } from "./components/ScamChecker";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex justify-center p-6">
-      <div className="w-full max-w-3xl">
-        <ScamChecker />
+    <>
+      <Header />
+      <div className="min-h-screen flex justify-center p-6 pt-2">
+        <div className="w-full max-w-3xl">
+          <ScamChecker />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
