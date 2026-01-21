71bbf0b20fc845207d7c6c340bde3bbf22f78b12
Abebdo <xjokerrgamer@gmail.com>
2026-01-21 13:06:56 +0200

    Delete src directory

--- src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loader() {
-  return (
-    <div className="flex justify-center my-4">
-      <div className="animate-spin rounded-full h-10 w-10 border-4 border-primary border-t-transparent"></div>
-    </div>
-  );
-}
